4c3736e09ea6e74c8d2211ddff4d998bdf802029
Justin Lupica <justinlupica@gmail.com>
2020-07-15 14:02:06 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
-
+def hello_t(array)
+name = 0
+  while name < array.length
+    yield(array[name])
+  name += 1
+    end
+array
+end
+hello_t(" ") do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
 
